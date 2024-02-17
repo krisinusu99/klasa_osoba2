b3d08242b136a49d94c0bcf186ccbf3634adc7eb
student <>
2024-02-17 23:29:01 +0000

    zmiany

--- klasa_osoba.py
+++ klasa_osoba.py
@@ -1,1 +1,10 @@
-class Osoba: 
+class Osoba: # Klasa osoba.
+    def__init__(self): # Metoda __init__
+       print("Obiekt klasy Osoba zosta utworzony.")
+       print()
+       print("Wprowadzanie danych do programu.")
+       print()
+    def__del__(self): #Metoda __del__
+       print()
+       print("Obiekt klasy Osoba zosta usuniety")
+       print("Zakonczenie programu=")
